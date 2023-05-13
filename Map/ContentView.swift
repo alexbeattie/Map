b3d97a4a57269467937ff21724801566e103cfa6
Alex Beattie <alexanderbeattie@mac.com>
2023-05-12 19:36:52 -0700

    Initial Commit

---
+++ Map/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Map
+//
+//  Created by Alex Beattie on 5/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Alex Beattie on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
